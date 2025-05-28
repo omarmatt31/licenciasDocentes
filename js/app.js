4d9ddb65431754199a379f29a7b2469da71db7c1
Omar Mattos <omarmatt84@gmail.com>
2025-05-27 22:24:33 -0300

    agregamos los metodos necesarios para agregar un nuevo docente en el localstorage

--- js/app.js
+++ js/app.js
@@ -5,7 +5,34 @@ const abrirModal = ()=>{
     modalDocente.show()
 }
 
+const crearDocente = ()=>{
+    const docenteNuevo = new Docente(inputApellido.value, inputNombre.value, inputCuil.value, inputFechaNac.value, inputTelefono.value)
+    //const docenteNuevo = new Docente(inputApellido.value, inputNombre.value, inputCuil.value, inputTelefono.value)
+    listadoDocente.push(docenteNuevo)
+    guardarLocalStorage()
+    limpiarFormulario()
+}
+
+const guardarLocalStorage = () =>{
+    localStorage.setItem('listadoDocenteKey', JSON.stringify(listadoDocente))
+}
+
+const limpiarFormulario = () =>{
+    formularioDocentes.reset()
+}
+
 const btnAgregar = document.getElementById('btnAgregar')
+const formularioDocentes = document.querySelector('form')
+const inputApellido = document.querySelector('#apellido')
+const inputNombre = document.querySelector('#nombre')
+const inputCuil = document.querySelector('#cuil')
+const inputFechaNac = document.querySelector('#fechaNac')
+const inputTelefono = document.querySelector('#telefono')
+const listadoDocente = JSON.parse(localStorage.getItem('listadoDocenteKey')) || []
 
 
 btnAgregar.addEventListener('click', abrirModal)
+formularioDocentes.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    crearDocente()
+})
