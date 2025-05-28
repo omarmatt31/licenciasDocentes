5f99b4fbd3e1cc51258c99cbc7e1b7074587eebc
Omar Mattos <omarmatt84@gmail.com>
2025-05-27 22:05:23 -0300

    agregamos la ventana modal para cargar los datos de docentes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-import Docente from "./classDocente";
+import Docente from "./classDocente.js"
 
+const abrirModal = ()=>{
+    const modalDocente = new bootstrap.Modal(document.getElementById('modalDocente'))
+    modalDocente.show()
+}
+
+const btnAgregar = document.getElementById('btnAgregar')
+
+
+btnAgregar.addEventListener('click', abrirModal)
