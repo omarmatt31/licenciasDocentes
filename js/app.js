4b51cd2f97d28a7b4432b55c6745a08c31df8815
Omar Mattos <omarmatt84@gmail.com>
2025-06-17 20:32:49 -0300

    se agregaron validaciones para el formulario modal

--- js/app.js
+++ js/app.js
@@ -9,23 +9,25 @@ const abrirModal = () => {
 };
 
 const crearDocente = () => {
-  const docenteNuevo = new Docente(
-    inputApellido.value,
-    inputNombre.value,
-    inputCuil.value,
-    inputFechaNac.value,
-    inputTelefono.value
-  );
-  //const docenteNuevo = new Docente(inputApellido.value, inputNombre.value, inputCuil.value, inputTelefono.value)
-  listadoDocente.push(docenteNuevo);
-  guardarLocalStorage();
-  dibujarFila(docenteNuevo, listadoDocente.length);
-  limpiarFormulario();
-  Swal.fire({
-    title: "Docente creado",
-    text: `El docente ${docenteNuevo.apellido} fue creado con exito`,
-    icon: "success",
-  });
+    if (validaciones()){
+        const docenteNuevo = new Docente(
+            inputApellido.value,
+            inputNombre.value,
+            inputCuil.value,
+            inputFechaNac.value,
+            inputTelefono.value
+        );
+        //const docenteNuevo = new Docente(inputApellido.value, inputNombre.value, inputCuil.value, inputTelefono.value)
+        listadoDocente.push(docenteNuevo);
+        guardarLocalStorage();
+        dibujarFila(docenteNuevo, listadoDocente.length);
+        limpiarFormulario();
+        Swal.fire({
+            title: "Docente creado",
+            text: `El docente ${docenteNuevo.apellido} fue creado con exito`,
+            icon: "success",
+        });
+    }
 };
 
 const guardarLocalStorage = () => {
@@ -106,19 +108,80 @@ window.verDocente = (id) => {
 };
 
 const editarDocente = () => {
-  const posicionDocente = listadoDocente.findIndex(
-    (docente) => docente.id === idDocenteEditar
-  );
-  listadoDocente[posicionDocente].apellido = inputApellido.value;
-  listadoDocente[posicionDocente].nombre = inputNombre.value;
-  listadoDocente[posicionDocente].cuil = inputCuil.value;
-  listadoDocente[posicionDocente].fechaDeNacimiento = inputFechaNac.value;
-  listadoDocente[posicionDocente].telefono = inputTelefono.value;
+    if(validaciones()){
+        const posicionDocente = listadoDocente.findIndex(
+            (docente) => docente.id === idDocenteEditar
+        );
+        listadoDocente[posicionDocente].apellido = inputApellido.value;
+        listadoDocente[posicionDocente].nombre = inputNombre.value;
+        listadoDocente[posicionDocente].cuil = inputCuil.value;
+        listadoDocente[posicionDocente].fechaDeNacimiento = inputFechaNac.value;
+        listadoDocente[posicionDocente].telefono = inputTelefono.value;
 
-  guardarLocalStorage();
-  limpiarFormulario();
-  abrirModal;
+        guardarLocalStorage();
+        limpiarFormulario();
+        abrirModal;
+    }
 };
+
+function validarCantidadCaracteres(input, min, max) {
+  if (input.value.trim().length >= min && input.value.trim().length <= max) {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  } else {
+    input.classList.add("is-invalid");
+     input.classList.remove("is-valid");
+    return false;
+  }
+}
+
+function validarCUILConGuiones(cuil) {
+    const regExp=/^(20|23|24|27|30|33|34)[-]?[0-9]{8}[-]?[0-9]$/
+    if(regExp.test(cuil)){
+        inputCuil.classList.add("is-valid");
+        inputCuil.classList.remove("is-invalid");
+        return true
+    }else {
+        inputCuil.classList.add("is-invalid");
+        inputCuil.classList.remove("is-valid")
+    }
+}
+
+function validarFechaNac(fecha){
+    const anioActual = new Date().getFullYear()
+    if((anioActual - fecha.getFullYear())>=22){
+        inputFechaNac.classList.add("is-valid");
+        inputFechaNac.classList.remove("is-invalid");
+        console.log("valido")
+        return true
+    }else {
+        inputFechaNac.classList.add("is-invalid");
+        inputFechaNac.classList.remove("is-valid");
+        console.log("invalido")
+        return false
+    }
+}
+
+function validaciones() {
+  let datosValidos = true;
+  if (!validarCantidadCaracteres(inputApellido, 2, 50)) {
+    datosValidos = false;
+  }
+
+if (!validarCantidadCaracteres(inputNombre, 2, 50)) {
+    datosValidos = false;
+  }
+
+  if (!validarCUILConGuiones(inputCuil.value)){
+    datosValidos = false;
+  }
+
+  if(!validarFechaNac(new Date(inputFechaNac.value))){
+    datosValidos = false;
+  }
+  return datosValidos;
+}
 
 const btnAgregar = document.getElementById("btnAgregar");
 const formularioDocentes = document.querySelector("form");
